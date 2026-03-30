dba939723675882f1f3da2f0d7bb325379f65458
Francesco <francesco.cataldo@spinforward.it>
2026-03-30 14:38:13 +0200

    fix(core): default port to 8000 (dev)

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo "Use -> python manage.py import_istat_data --force"
 
 echo "Starting Gunicorn..."
 exec gunicorn config.wsgi:application \
---bind 0.0.0.0:$PORT \
+--bind 0.0.0.0:${PORT:-8000} \
 --workers 2 \
 --timeout 120 \
 --access-logfile - \
